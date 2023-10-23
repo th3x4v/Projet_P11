0eb77b08afc0bd21f80c0fd0c74b2818d50a1039
Xavier Lonjon <xavierlonjon@yahoo.fr>
2023-10-23 17:30:37 +0200

    new FEATURE: Implement Points Display Board

--- server.py
+++ server.py
@@ -3,6 +3,16 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
+def is_competition_available(competition):
+    now = datetime.now()
+    comp_date = datetime.strptime(competition["date"], "%Y-%m-%d %H:%M:%S")
+    if comp_date < now:
+        competition["available"] = False
+    else:
+        competition["available"] = True
+    return competition
+
+
 def loadClubs():
     with open("clubs.json") as c:
         listOfClubs = json.load(c)["clubs"]
@@ -12,7 +22,9 @@ def loadClubs():
 def loadCompetitions():
     with open("competitions.json") as comps:
         listOfCompetitions = json.load(comps)["competitions"]
-        return listOfCompetitions
+        # Checking if competition is past or not
+        _listOfCompetitions = [is_competition_available(c) for c in listOfCompetitions]
+        return _listOfCompetitions
 
 
 app = Flask(__name__)
@@ -27,31 +39,32 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/showSummary", methods=["POST"])
+@app.route("/showSummary", methods=["POST", "GET"])
 def showSummary():
-    try:
-        club = [c for c in clubs if c["email"] == request.form["email"]][0]
+    if request.method == "POST":
+        try:
+            club = [c for c in clubs if c["email"] == request.form["email"]][0]
+            return render_template("welcome.html", club=club, competitions=competitions)
 
-        # Checking if competition is past or not, then render template
-        for c in competitions:
-            print(int(c["numberOfPlaces"]))
-        _competitions = [is_competition_available(c) for c in competitions]
+        except IndexError:
+            print("error club")
+            flash("Please enter a valid email")
+            return redirect(url_for("index"))
 
-        return render_template("welcome.html", club=club, competitions=_competitions)
-
-    except IndexError:
-        flash("Please enter a valid email")
-        return redirect(url_for("index"))
+    if request.method == "GET":
+        # Retrieve the club data
+        club_name = request.args.get("club")
+        club = [c for c in clubs if c["name"] == club_name][0]
+        if club is not None:
+            return render_template("welcome.html", club=club, competitions=competitions)
+        else:
+            return redirect(url_for("index"))
 
 
 @app.route("/book/<competition>/<club>")
 def book(competition, club):
     foundClub = [c for c in clubs if c["name"] == club][0]
     foundCompetition = [c for c in competitions if c["name"] == competition][0]
-    # The line `if competition[0]["available"] is False:` is checking if the "available" key in the first
-    # element of the `competition` list is `False`.
-    print("competition")
-    print(foundCompetition)
     if foundCompetition["available"] is False:
         flash("This competition is not available")
         return render_template("welcome.html", club=club, competitions=competitions)
@@ -73,6 +86,7 @@ def purchasePlaces():
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     club_points = int(club["points"])
     placesRequired = int(request.form["places"])
+    placesAvailble = int(competition["numberOfPlaces"])
     if competition["available"] is False:
         flash("This competition is not available")
         return render_template("welcome.html", club=club, competitions=competitions)
@@ -82,6 +96,11 @@ def purchasePlaces():
     elif placesRequired > club_points:
         flash(
             f"You don't have enough points ({club_points}) to reedem {placesRequired} places"
+        )
+        return render_template("welcome.html", club=club, competitions=competitions)
+    elif placesRequired > placesAvailble:
+        flash(
+            f"You are trying to reedem  places ({placesRequired}) than available ({placesAvailble}"
         )
         return render_template("welcome.html", club=club, competitions=competitions)
     else:
@@ -96,7 +115,10 @@ def purchasePlaces():
         return render_template("welcome.html", club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
+@app.route("/board")
+def board():
+    current_club_name = request.args.get("club")
+    return render_template("board.html", club_name=current_club_name, clubs=clubs)
 
 
 @app.route("/logout")
---
+++ tests/unit_tests/test_board.py
@@ -0,0 +1,11 @@
+def test_correct_points_should_display(client, clubs, monkeypatch):
+    """
+    GIVEN a request on the board page
+    WHEN the '/board' page get the request (GET)
+    THEN check is the status code returned is 200, and if a text is in the response. Check if correct clubs/amounts of points displays correctly
+    """
+    monkeypatch.setattr("server.clubs", clubs)
+    response = client.get("/board")
+    assert response.status_code == 200
+    assert clubs[0]["name"] in response.data.decode()
+    assert clubs[0]["points"] in response.data.decode()
